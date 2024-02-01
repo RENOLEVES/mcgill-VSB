81850af780a351532d38589096a9500e5d5baf23
renoleves <zzhao260985652@gmail.com>
2024-01-31 20:18:31 -0500

    1/31

---
+++ src/main/java/com/mcgillschedule/mcgilltimetablebuilder/model/backend/TimetableResultInfo.java
@@ -0,0 +1,32 @@
+package com.mcgillschedule.mcgilltimetablebuilder.model.backend;
+
+import java.util.List;
+
+
+
+import java.util.List;
+
+public class TimetableResultInfo {
+    private Integer totalTimetableSize;
+    private List<List<CourseInfo>> topTimetables;
+
+    public TimetableResultInfo() {
+    }
+    public TimetableResultInfo(Integer totalTimetableSize, List<List<CourseInfo>> topTimetables) {
+        this.totalTimetableSize = totalTimetableSize;
+        this.topTimetables = topTimetables;
+    }
+
+    public Integer getTotalTimetableSize() {
+        return totalTimetableSize;
+    }
+    public void setTotalTimetableSize(Integer totalTimetableSize) {
+        this.totalTimetableSize = totalTimetableSize;
+    }
+    public List<List<CourseInfo>> getTopTimetables() {
+        return topTimetables;
+    }
+    public void setTopTimetables(List<List<CourseInfo>> topTimetables) {
+        this.topTimetables = topTimetables;
+    }
+}
---
+++ src/main/java/com/mcgillschedule/mcgilltimetablebuilder/model/backend/TimetableWithScore.java
@@ -0,0 +1,37 @@
+package com.mcgillschedule.mcgilltimetablebuilder.model.backend;
+
+import java.util.List;
+
+public class TimetableWithScore implements Comparable<TimetableWithScore> {
+    private List<CourseInfo> timetable;
+    private double score;
+
+    public TimetableWithScore(List<CourseInfo> timetable, double score) {
+        this.timetable = timetable;
+        this.score = score;
+    }
+
+    // Getters
+
+    public List<CourseInfo> getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(List<CourseInfo> timetable) {
+        this.timetable = timetable;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(TimetableWithScore other) {
+        // 按分数降序排列
+        return Double.compare(other.score, this.score);
+    }
+}
